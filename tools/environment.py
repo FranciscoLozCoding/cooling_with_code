5ad67eb47c44c55bc352e7c03bb6a8d0139b8bec
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-03-05 22:51:48 -0600

    Add buffer distances and target variable definitions to environment settings

--- tools/environment.py
+++ tools/environment.py
@@ -5,3 +5,9 @@ RANDOM_STATE=123
 
 # used for defining the validation size in train_test_split
 VALID_SPLIT=0.3
+
+# defines the different buffer distances we used for the project
+BUFFER_DISTANCES=[50, 100, 150, 300]
+
+# defines the target variable for the project
+TARGET_VARIABLE='UHI'
