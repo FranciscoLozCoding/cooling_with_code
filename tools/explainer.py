fix: adjust SHAP value computation to handle three-dimensional arrays

tools/explainer.py
@@ -43,7 +43,10 @@ class Explainer:
             ref_data = np.array(ref_data) if isinstance(ref_data, pd.DataFrame) else ref_data # Ensure NumPy format
         self.feature_names = feature_names
         self.explainer = explainer_type(model, ref_data)  # Initialize explainer
-        self.shap_values = self.explainer.shap_values(self.X)  # Compute SHAP values
+        self.shap_values = self.explainer.shap_values(self.X, check_additivity=False)  # Compute SHAP values
+        # Apply squeeze only if the array has three dimensions and the last dimension is 1
+        if self.shap_values.ndim == 3 and self.shap_values.shape[-1] == 1:
+            self.shap_values = np.squeeze(self.shap_values)
         self.init_js()  # Initialize SHAP for Jupyter Notebook
 
     def summary_plot(self):