7dc1007622cd776876926cdf32358435412c6092
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-03-14 10:34:28 -0500

    added new env var

--- tools/environment.py
+++ tools/environment.py
@@ -11,3 +11,13 @@ BUFFER_DISTANCES=[50, 100, 150, 300]
 
 # defines the target variable for the project
 TARGET_VARIABLE='UHI'
+
+# defines the selected features for Mixed Buffers ResNet model
+MixBuf_ResNet_SelectAttr=['50m_1NPCRI', '100m_Elevation_Wind_X', '150m_Traffic_Volume',
+                        '150m_Elevation_Wind_Y', '150m_Humidity_NDVI', '150m_Traffic_NDBI',
+                        '300m_SI', '300m_NPCRI', '300m_Coastal_Aerosol', '300m_Total_Building_Area_m2',
+                        '300m_Building_Construction_Year', '300m_Ground_Elevation', '300m_Building_Wind_X',
+                        '300m_Building_Wind_Y', '300m_Elevation_Wind_Y', '300m_BldgHeight_Count',
+                        '300m_TotalBuildingArea_NDVI', '300m_Traffic_NDVI', '300m_Traffic_NDBI',
+                        '300m_Building_Aspect_Ratio', '300m_Sky_View_Factor', '300m_Canopy_Cover_Ratio',
+                        '300m_GHG_Proxy']
