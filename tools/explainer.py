f85aabbb6fcd298c59e6c3aa4c269cdc145a4254
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-03-17 10:19:56 -0500

    Refactor expected_value assignment for clarity in Explainer class

--- tools/explainer.py
+++ tools/explainer.py
@@ -103,13 +103,11 @@ class Explainer:
             dict: The insights for the selected record.
         """
 
-        # if self.explainer_type == shap.DeepExplainer:
-        #     # Ensure expected_value is a single value (not tensor)
-        #     expected_value = np.array(self.explainer.expected_value)[0]
-        # else:
-        #     expected_value = self.explainer.expected_value
-
-        expected_value = self.explainer.expected_value
+        if self.explainer_type == shap.DeepExplainer:
+            # Ensure expected_value is a single value (not tensor)
+            expected_value = np.array(self.explainer.expected_value)[0]
+        else:
+            expected_value = self.explainer.expected_value
 
         # Validate record index
         if index >= len(self.shap_values) or index < 0:
